dfef997ec3f75c402f70aae5f48e927446008dad
Jack Stone <jackstonemedia@gmail.com>
2017-07-18 20:01:18 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -22,27 +22,34 @@ socket.on('disconnect', function() {
 // });
 
 socket.on('newMessage', function(message) {
-  // console.log('newMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  // Mustache Template //
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    // render properties //
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  // Mustache Template //
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    // render properties //
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 
